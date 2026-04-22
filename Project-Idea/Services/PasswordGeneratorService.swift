b60b33e3c69d3d4bdb10401c8dd359ee235a4743
TomBrophy101 <tombrophy101@outlook.com>
2026-04-22 13:52:50 +0100

    New Password Generator.

---
+++ Project-Idea/Services/PasswordGeneratorService.swift
@@ -0,0 +1,28 @@
+//
+//  PasswordGeneratorService.swift
+//  Project-Idea
+//
+//  Created by Tom Brophy on 22/04/2026.
+//
+import Foundation
+
+struct PasswordGeneratorService {
+    static func generate(length: Int = 16, includeSymbols: Bool = true, includeNumbers: Bool = true) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let digits = "0123456789"
+        let symbols = #"!€@£#$%^&*()-_=+[]{}|;:,.<>?/\"#
+
+        var characterSet = letters
+        if includeNumbers {
+            characterSet += digits
+        }
+
+        if includeSymbols {
+            characterSet += symbols
+        }
+
+        return String((0..<length).map {_ in
+            characterSet.randomElement() ?? "x"
+        })
+    }
+}
