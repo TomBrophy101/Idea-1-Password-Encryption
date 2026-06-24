3c75bf9e09d7e70ae3d0dca714daf41196fe3c62
TomBrophy101 <tombrophy101@outlook.com>
2026-06-24 12:25:25 +0100

    Right keyTag name.

--- SalusPass/Services/KeychainManager.swift
+++ SalusPass/Services/KeychainManager.swift
@@ -11,7 +11,7 @@ import CryptoKit
 
 struct KeychainManager {
     static func getOrCreateMasterKey() -> SymmetricKey {
-        let keyTag = "com.projectidea.masterKey"
+        let keyTag = "com.saluspass.masterKey"
 
         if let existingKeyData = loadData(key: keyTag) {
             return SymmetricKey(data: existingKeyData)
